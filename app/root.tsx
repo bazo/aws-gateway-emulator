c6d1adde7e19db0f63659f236b680873312baf22
Martin Bazik <martin@bazo.sk>
2023-11-08 18:53:04 +0100

    add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -18,6 +18,11 @@ export default function App() {
 				{/* All meta exports on all routes will go here */}
 				<Meta />
 
+				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+				<link rel="manifest" href="/site.webmanifest" />
+
 				{/* All link exports on all routes will go here */}
 				<Links />
 			</head>
